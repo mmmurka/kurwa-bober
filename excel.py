58c1764f4c7c7888a75fb7ceb69910e4efac6dc8
mmmurka <murenkodan2005@gmail.com>
2024-11-02 12:16:44 +0200

    added excel

---
+++ excel.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from varus_selenium import search_product_with_selenium
+
+file_path = 'h.xlsx'
+df = pd.read_excel(file_path)
+pd.set_option('display.max_colwidth', None)
+
+column_data = df['Unnamed: 2'].tolist()
+for name in column_data[1:]:
+    search_product_with_selenium(name)
+
+
