701fbd8ad44494f6a43ba18097b7fee79d563300
Nemsh <Artnemshilov@gmail.com>
2024-11-03 08:18:50 +0100

    t

--- main.py
+++ main.py
@@ -14,18 +14,18 @@ from shop.makeup import search_product_makeup
 #     search_product_makeup(original_name, hashcodes)
 
 # Path to the prod.xlsx file
-file_path = 'hh.xlsx'
+file_path = 'prod.xlsx'
 
 def update_excel_with_shop_data():
     df = pd.read_excel(file_path)
 
-    products = [product for product in df['Unnamed: 2'].iloc[1:5]]
+    products = [product for product in df['Unnamed: 2'].iloc[1:-1]]
     hashcodes = [hashcode for hashcode in df['Unnamed: 1'].iloc[1:-1]]
 
     for index, product in enumerate(products, start=1):
         varus_product = search_product_varus(product)
         # silpo_product = search_product_silpo(product)
-        # makeup_product = search_product_makeup(product, hashcodes)
+        makeup_product = search_product_makeup(product, hashcodes)
         # eva_product = search_product_eva(product)
         #
         # if silpo_product:
@@ -42,12 +42,12 @@ def update_excel_with_shop_data():
         #     df.at[index, 'Unnamed: 6'] = eva_product.get('match', '')
         #     print(f"EVA: {eva_product} внесено")
         #
-        # if makeup_product:
-        #     df.at[index, 'makeup'] = makeup_product.get('price')
-        #     df.at[index, 'Unnamed: 12'] = makeup_product.get('old_price') if makeup_product.get('old_price') is not None else makeup_product.get('price')
-        #     df.at[index, 'Unnamed: 13'] = makeup_product.get('link', '')
-        #     df.at[index, 'Unnamed: 14'] = makeup_product.get('match', '')
-        #     print(f"Makeup: {makeup_product} внесено")
+        if makeup_product:
+            df.at[index, 'makeup'] = makeup_product.get('price')
+            df.at[index, 'Unnamed: 12'] = makeup_product.get('old_price') if makeup_product.get('old_price') is not None else makeup_product.get('price')
+            df.at[index, 'Unnamed: 13'] = makeup_product.get('link', '')
+            df.at[index, 'Unnamed: 14'] = makeup_product.get('match', '')
+            print(f"Makeup: {makeup_product} внесено")
         if varus_product:
             df.at[index, 'varus'] = varus_product.get('price')
             df.at[index, 'Unnamed: 16'] = varus_product.get('old_price') if varus_product.get('old_price') is not None else varus_product.get('price')
--- shop/varus.py
+++ shop/varus.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
 from utils.functions import similarity_ratio
 from utils.driver import DriverSingleton
 
@@ -10,51 +9,53 @@ varus_link = 'https://varus.ua/search?q='
 def search_product_varus(product_name):
     driver = DriverSingleton.get_driver()
     words = product_name.split()
+
     try:
         while words:
             search_url = varus_link + '%20'.join(words)
             driver.get(search_url)
 
             try:
-                WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, 'sf-product-card__block'))
+                # Ожидание загрузки карточек продуктов
+                WebDriverWait(driver, 2).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, 'sf-product-card__link'))
                 )
+
+                # Извлечение ссылки с помощью selenium
+                product_link_tag = driver.find_element(By.CLASS_NAME, 'sf-product-card__link')
+                link = product_link_tag.get_attribute('href')
+
+                # Проверка и корректировка ссылки
+                if link and not link.startswith('http'):
+                    link = 'https://varus.ua' + link
+
+                # Извлечение названия и цены
+                title = driver.find_element(By.CLASS_NAME, 'sf-product-card__title').text.strip()
+                price = driver.find_element(By.CLASS_NAME, 'sf-price__regular').text.strip().replace(' грн', '')
+
+                # Проверка старой цены
+                try:
+                    old_price = driver.find_element(By.CLASS_NAME, 'sf-price__old').text.strip().replace(' грн', '')
+                except:
+                    old_price = None
+
+                # Подсчет схожести
+                similarity = similarity_ratio(product_name, title)
+
+                return {
+                    'price': price,
+                    'old_price': old_price,
+                    'match': similarity,
+                    'link': link
+                }
+
             except Exception:
+                # Если элемент не найден, удаляем последнее слово и продолжаем без вывода ошибок
                 words.pop()
                 continue
 
-            soup = BeautifulSoup(driver.page_source, 'html.parser')
-            product = soup.find('div', class_='sf-product-card__block')
+        # Если после всех итераций товар не найден, возвращаем None
+        return None
 
-            if product:
-                title = product.find('a', class_='sf-product-card__title')
-                price = product.find('span', class_='sf-price__special')
-                old_price = product.find('span', class_='sf-price__old')
-
-                product_link_tag = product.find('a', class_='sf-link.sf-product-card__link.sf-product-card__title-wrapper')
-                link = product_link_tag['href'] if product_link_tag else None
-
-                if title and price:
-                    title_text = title.text.strip()
-                    similarity = similarity_ratio(product_name, title_text)
-
-                    if old_price:
-                        return {
-                            'price': price.text.strip()[:-3],
-                            'old_price': old_price.text.strip()[:-3],
-                            'match': similarity,
-                            'link': link
-                        }
-                    else:
-                        return {
-                            'price': price.text.strip()[:-3],
-                            'match': similarity,
-                            'link': link
-                        }
-                else:
-                    words.pop()
-
-                if not words:
-                    return None
     finally:
         DriverSingleton.quit_driver()
