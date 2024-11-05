8858baa195fa86d266d0f2174263373bea45f6d8
Nemsh <Artnemshilov@gmail.com>
2024-11-05 23:19:10 +0100

    rebasing

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import asyncio
 
 import pandas as pd
 
-from shop.silpo import search_product_silpo
+from shop.silpo import search_silpo_product
 
 df = pd.read_excel('prodick.xlsx')
 
 async def write_prices():
     for product in df['Unnamed: 2'].iloc[5:10]:
-        silpo_product = await search_product_silpo(product)
+        silpo_product = await search_silpo_product(product)
 
         print(silpo_product)
 
